6174ff1910c48ce8782df89fbb024b9e893bf5b7
sawe-daisy <deesawe16@gmail.com>
2020-10-08 11:54:24 +0300

    profiles

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
+import { User } from '../user';
+import { HttpClient } from '@angular/common/http';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  user: User;
+  users = [];
+  repo: Repo;
+  repos = [];
 
-  constructor() { }
+  constructor(public serviceService: ServiceService, http: HttpClient) {}
 
-  ngOnInit(): void {
+  performSearch(searchTerm): any {
+
+    this.serviceService.getProfileInfo(searchTerm).then(
+      (result) => {
+        this.user = this.serviceService.user;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.serviceService.getRepoInfo(searchTerm).then(
+      (result) => {
+        this.repo = this.serviceService.repo;
+        this.repos.push(this.repo);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  ngOnInit(): void {
+    this.performSearch('sawe-daisy');
+  }
 }
