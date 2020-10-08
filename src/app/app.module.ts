daa7e69f960e8bb2bea9d62062451adadfbe6258
sawe-daisy <deesawe16@gmail.com>
2020-10-08 11:07:34 +0300

    search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProfileComponent,
+    NotFoundComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
